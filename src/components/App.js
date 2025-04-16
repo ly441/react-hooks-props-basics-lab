90e422992b5405ecba13ce1996dd4b94a97b96c2
Lynn <koliilynn@gmail.com>
2025-04-16 18:39:26 +0300

    added a link.js file

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,15 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+import Links from "./Links";
+console.log(Links);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About github={user.links.github} linkendin={user.links.linkedin} bio={user.bio} />
     </div>
   );
 }
